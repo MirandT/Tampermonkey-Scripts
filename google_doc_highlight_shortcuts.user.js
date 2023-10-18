5102c5e3017363ff9fb97d750e53143c77f462f5
MirandT <61469570+MirandT@users.noreply.github.com>
2023-10-18 10:09:34 -0600

    Update google_doc_highlight_shortcuts.user.js

--- google_doc_highlight_shortcuts.user.js
+++ google_doc_highlight_shortcuts.user.js
@@ -1,151 +1,70 @@
-// to install this script: install the tampermonkey extension. Then click on "raw" on this github page.
-
-// *perso note:
-    //to install or to send update:
-    // 1/ edit must be done on github (cf. the url in the tab settings).
-    // 2/ Edit/update the script (using the edit github button) + increase the version (or it will ask to reinstall in the next step)
-        // (don't need to increase the version if the change are just for myself: just reinstall it to keep the version)
-    // 3/ click on RAW (it will show the changes).
-    // 4/ Click Update (if needed refresh the dashboard page)
-
-    // last update: 2020_02_27. 
-    // New features: 
-    // 2020_02_27: color in red the current title in the outline panel
-   
-
-
 // ==UserScript==
-// @name Google Docs Tweaks
-// @namespace some_google_doc_shortcuts_to_change_font_highlight
-// @version 1.0.5
-// @author Willi
-// @description some google doc shortcuts to change the font and highlights (help needed for font size!). It also hide the page break dotted lines.
-
-// @include      https://*docs.google.*/document/*
-// @require    http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-
-// add a style sheet to the page (needed to tweak CSS) https://stackoverflow.com/a/19392142/1486850
+// @name        Google Doc Highlight Shortcuts
+// @namespace   google_doc_highlight_shortcuts
+// @version     1.0
+// @author      Me
+// @include     https://*docs.google.*/document/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @grant    GM_addStyle
-
 // ==/UserScript==
 
-console.log('ok, tamperstarted !');
+console.log('Script Google Doc Highlight Shortcuts Started!');
 
-// hide the page break doted line:
-// https://stackoverflow.com/a/52870899/3154274
-GM_addStyle ( `
-.kix-page-compact::before {
-        border-top: none;
-    }
-// color in red current title in outline
-.outline-refresh .location-indicator-highlight.navigation-item,  .outline-refresh .location-indicator-highlight.navigation-item.goog-button-hover {
-color: rgb(255, 0, 0);
-}
-` );
-
-
- // listen for key shorcuts on the text part of google gocs
-//	● Tuto - sources:
-// 		○ for iframe https://stackoverflow.com/a/46217408/3154274
-// 		○ for switch https://stackoverflow.com/q/13362028/3154274
-// 		○ combinaison of key  https://stackoverflow.com/a/37559790/3154274
-// 		○ dispatchEvent https://stackoverflow.com/a/33887557/3154274
-
-// 		○ for dispatch :
-//		    https://jsfiddle.net/6vyL98mz/33/
-//    		https://jsfiddle.net/ox2La621/1/
-
-var editingIFrame = $('iframe.docs-texteventtarget-iframe')[0];
-if (editingIFrame) {
-    editingIFrame.contentDocument.addEventListener("keydown", dispatchkeyboard, false);
-}
-
-
-// match the key with the action
-function dispatchkeyboard(key) {
-    if (key.ctrlKey) {
+document.addEventListener("keydown", function (key) {
+	if (key.ctrlKey) {
         return;
     }
-
-    //--------------- background color
-
-    // Remove BG color--no idea why it needs two callMouseEvents
-    if (key.altKey  &&  key.code === "KeyR") {
-        var buttonbg = document.getElementById("bgColorButton");
-
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementsByClassName("goog-menuitem colormenuitems-no-color")[0];
-            console.log(color_choice);
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-            callMouseEvent(color_choice);
-        }, 1);
-    }
-
-    // hl light purple 3
-    if (key.altKey  &&  key.code === "KeyB") {
-        var buttonbg = document.getElementById("bgColorButton");
-
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementById("docs-material-colorpalette-cell-118");
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-        }, 1);
-    }
-
-    // hl neon green
-    if (key.altKey  &&  key.code === "KeyG") {
-        var buttonbg = document.getElementById("bgColorButton");
-
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementById("docs-material-colorpalette-cell-104");
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-        }, 1);
-    }
-
-    // hl light green 3
-    if (key.altKey  &&  key.code === "KeyQ") {
-        var buttonbg = document.getElementById("bgColorButton");
-
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementById("docs-material-colorpalette-cell-114");
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-        }, 1);
-    }
     
-    //hl light cornflower blue 3
-    if (key.altKey  &&  key.code === "KeyL") {
-        var buttonbg = document.getElementById("bgColorButton");
+	// Create a new comment element
+	var commentElement = document.createElement("div");
+	commentElement.className = "docs-comment docs-reaction";
 
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementById("docs-material-colorpalette-cell-116");
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-        }, 1);
-    }
-    
-    //hl magenta
-    if (key.altKey  &&  key.code === "KeyP") {
-        var buttonbg = document.getElementById("bgColorButton");
+	// Set the comment element's text to the desired emoji
+	commentElement.textContent = "👍";
 
-        callMouseEvent(buttonbg);
-        setTimeout(function(){
-            var color_choice = document.getElementById("docs-material-colorpalette-cell-109");
-            console.log("clickbutton wait 2sec");
-            callMouseEvent(color_choice);
-        }, 1);
-    }
+	// Store the comment element in the Tampermonkey storage
+	GM_setValue("emojiReaction", commentElement);
 
-}// end of  dispatchkeyboard
-
-
+	// Click highlighter button
+	callMouseEvent(document.getElementById("bgColorButton"));
+	// Allow process time to click highlighter button
+	setTimeout(function(){
+		switch (key.altKey  &&  key.code) {
+			case "KeyL":
+				// Select light cornflower blue 3 color
+				callMouseEvent(document.getElementById("docs-material-colorpalette-cell-116"));
+				break;
+			case "KeyP":
+				// Select magenta color
+				callMouseEvent(document.getElementById("docs-material-colorpalette-cell-109"));
+				break;
+			case "KeyQ":
+				// Select light green 3 color
+				callMouseEvent(document.getElementById("docs-material-colorpalette-cell-114"));
+				break;
+			case "KeyG":
+				// Select neon green color
+				callMouseEvent(document.getElementById("docs-material-colorpalette-cell-104"));
+				break;
+			case "KeyB":
+				// Select light purple 3 color
+				callMouseEvent(document.getElementById("docs-material-colorpalette-cell-118"));
+				break;
+			case "KeyR":
+				// Select remove color
+				callMouseEvent(document.getElementsByClassName("goog-menuitem colormenuitems-no-color")[0]);
+				break;
+			case "KeyS":
+				console.log("Start emoji comment 2.0");
+				// Get the comment element from the Tampermonkey storage
+				var getCommentElement = GM_getValue("emojiReaction");
+				// Inject the comment element into the document
+				document.querySelector(".docs-editor-container").appendChild(getCommentElement);
+				console.log("End emoji comment");
+				break;
+		}
+	}, 1);
+});
 //call each mouse event
 function callMouseEvent(button){
     triggerMouseEvent (button, "mouseover");
@@ -155,7 +74,7 @@ function callMouseEvent(button){
 
 // send mouse even
 function triggerMouseEvent (node, eventType) {
-    var eventObj        = document.createEvent('MouseEvents');
+    var eventObj = document.createEvent('MouseEvents');
     eventObj.initEvent (eventType, true, true);
-    node.dispatchEvent   (eventObj);
+    node.dispatchEvent (eventObj);
 }
